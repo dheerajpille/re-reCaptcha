eedc9145fe160a7b93e2d3df15dd7b32ff4c634e
root <root@localhost.localdomain>
2018-07-14 17:35:59 -0400

    Fixed iframe not covering entire page

--- contentScript.js
+++ contentScript.js
@@ -1,7 +1,7 @@
 var f = document.createElement('iframe');
 f.src = chrome.extension.getURL('popup.html');
 f.setAttribute('id', 'iframe');
-f.style = 'position:absolute; top:0px; left:0px; bottom:0px; right:0px; width:100%; height:100%; border:none; margin:0; padding:0; overflow:hidden; z-index:999999;';
+f.style = 'position:fixed; top:0px; left:0px; bottom:0px; right:0px; width:100%; height:100%; border:none; margin:0; padding:0; overflow:hidden; z-index:999999;';
 document.body.appendChild(f);
 window.addEventListener('message', function(event) { 
   if (event.data === 'pass') {
